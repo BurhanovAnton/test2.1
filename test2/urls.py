431dd8cd1c166e84047074e412fe4f235d185d3b
Anton <Burhanov08@mail.ru>
2021-07-03 18:29:56 +0400

    проект

--- test2/urls.py
+++ test2/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mane import views as mane_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mane_views.main)
 ]

2b37d34f85d59a2d5aebfd795a9d3194363ef6ac
Anton <Burhanov08@mail.ru>
2021-07-12 22:52:09 +0400

    [#2] 1. Вроде все сделал. не уверен что все запомнил, но все понял, посмотреть тоже есть где. С видео очень удобно!

---
+++ mane/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from mane import views as mane_views
+
+app_name = 'album'
+
+urlpatterns = [
+    path('photo/<pk>/', mane_views.photo, name='photo'),
+    path('', mane_views.album, name='album')
+]
+
--- test2/urls.py
+++ test2/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 from mane import views as mane_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', mane_views.main),
-    path('photo/', mane_views.photo),
-    path('album/', mane_views.album),
+    path('', mane_views.main, name='index'),
+    path('album/', include('mane.urls', namespace='album')),
 ]

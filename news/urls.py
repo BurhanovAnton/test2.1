6bf801e35bc44569135abbd16c0d3c72f7e2374b
Anton <Burhanov08@mail.ru>
2021-07-27 22:25:29 +0400

    [#4] готово

--- news/urls.py
+++ news/urls.py
@@ -5,7 +5,6 @@ from news import views as news_views
 app_name = 'news'
 
 urlpatterns = [
-    path('news/', news_views.news_list, name='news_list'),
-    path('news/<pk>/', news_views.news_item, name='news_item'),
+    path('news/', news_views.news_list_all, name='news_list'),
+    path('news_item/<pk>/', news_views.news_item, name='news_item'),
 ]
-
